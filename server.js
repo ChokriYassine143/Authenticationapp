2b2b5321cbed6e034839362063a385b631b381ad
Chokri Yassine <123024506+ChokriYassine143@users.noreply.github.com>
2023-08-26 10:27:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
+  name: 'MyauthenticationCookieName',
   proxy: true,
   cookie:{
      httpOnly: true,
